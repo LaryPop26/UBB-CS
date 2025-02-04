49fd074dc796fafea403a8e0071b40543e495abb
LaryPop26 <149674363+LaryPop26@users.noreply.github.com>
2025-02-04 23:56:39 +0200

    more work

--- Third Semester/Logic and functional programming/Lisp/L2/4.lisp
+++ Third Semester/Logic and functional programming/Lisp/L2/4.lisp
@@ -21,25 +21,4 @@
 (print (transformare '(A (B (D (G)) (E (H (L (M) (N))) (I))) (C (F (J) (K (Q (P (R) (S)))))))))
 
 
-;;  Definiti o functie care inlocuieste un nod cu altul intr-un arbore n-ar reprezentat sub forma (radacina lista_noduri_subarb1...lista_noduri_subarbn)  
-;;  Ex: arborelele este (a (b (c)) (d) (e (f)))  si nodul 'b se inlocuieste cu nodul 'g => arborele (a (g (c)) (d) (e (f)))
-
-;; substituire (x, e , s) = x, x- atom, x != e
-;;                          s, x- atom, x = e
-;;                          U(i=1) (n) substituire(xi,e,s), x - lista, x = x1x2...xn
-
-(defun substituire(x e s)
-  (cond
-    ((and (atom x) (equal x e)) s)
-    ((atom x) x)
-    (t (mapcar #'(lambda (x) (substituire x e s)) x))
-    )
-  )
-
-(print (substituire '(a (b (c)) (d) (e (f))) 'b 'g))
-
-(print (substituire '(A (B (D (G)) (E (H (L (M) (N))) (I))) (C (F (J) (K (Q (P (R) (S))))))) 'E 'X))
-
-(print (substituire '(a (b (c)) (d) (e (f))) 'x 'g))
-
 
